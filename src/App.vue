d1ce1c50b02a4648b2bf3bfcd823d61b57fcc2e8
chenglie33 <394699890@qq.com>
2019-11-29 16:30:37 +0800

    首次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,128 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <el-container id="bodyMain">
+    <!-- <el-header class='color-primary'>
+      <h3>国际化文件导出程序</h3>
+      
+    </el-header>-->
+    <el-container class="overflowh">
+      <el-aside width="80px">
+        <el-row class="tac">
+          <el-col>
+            <el-menu
+              default-active="2"
+              background-color="#58B7FF"
+              text-color="#dfdfdf"
+              active-text-color="#fff"
+            >
+              <el-menu-item index="2">
+                <router-link class="router-link" tag="div" to="/"
+                  ><i
+                    class="el-icon-printer"
+                  /><br /><br />国际化工具</router-link
+                >
+
+                <!-- <span slot="title"></span> -->
+              </el-menu-item>
+              <el-menu-item index="3">
+                <router-link class="router-link" tag="div" to="/about"
+                  ><i class="el-icon-chat-round" /><br /><br />关于</router-link
+                >
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main style="display:flex">
+        <router-view />
+      </el-main>
+    </el-container>
+
+    <el-footer height="20px" class="tal">v1.0.0</el-footer>
+  </el-container>
 </template>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+body,
+html {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  font-size: 12px;
+  line-height: 12px !important;
+  #bodyMain {
+    height: 100%;
+  }
+}
+.el-menu-item,
+.el-submenu__title {
+  line-height: 12px !important;
+}
+a {
+  text-decoration: none;
+}
+.router-link-active {
+  text-decoration: none;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.el-header,
+.el-footer {
+  background-color: #20a0ff;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.el-aside {
+  background-color: #58b7ff;
+  color: #fff;
+  text-align: center;
+  line-height: 200px;
+}
+.el-menu-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.el-main {
+  // background-color: #e9eef3;
+  // color: #333;
+  text-align: center;
+}
+.el-icon-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  line-height: 12px;
+}
+.router-link {
+  font-size: 12px;
+}
+.fonts {
+  font-size: 12px;
+  font-weight: 400;
+}
+.el-checkbox__label {
+  font-size: 12px !important;
+  font-weight: 400;
+}
+.tal {
+  text-align: left;
+  line-height: 20px;
+  padding-left: 8px;
+}
+.overflowh {
+  overflow: hidden;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,251 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home" v-loading="loading">
+    <div>
+      <el-row :gutter="20" class="mb">
+        <el-col :span="5">
+          <div class="grid-content bg-purple">关键key：</div>
+        </el-col>
+        <el-col :span="15">
+          <div class="grid-content bg-purple">
+            <el-input
+              v-model="keyv"
+              placeholder="输入excel中Row的key(默认为A)"
+            ></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" class="mb">
+        <el-col :span="5">
+          <div class="grid-content bg-purple">是否使用正则：</div>
+        </el-col>
+        <el-col :span="15">
+          <div class="grid-content bg-purple">
+            <el-switch
+              v-model="isusereg"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            >
+            </el-switch>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" class="mb" v-if="isusereg">
+        <el-col :span="5">
+          <div class="grid-content bg-purple">匹配规则：</div>
+        </el-col>
+        <el-col :span="15">
+          <div class="grid-content bg-purple">
+            <el-input
+              v-model="keyreg"
+              placeholder="正则表达式 默认&符 导出名称以&之前的为准"
+            ></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row class="mb">
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
+            <el-upload
+              class="upload-demo"
+              action=""
+              :before-upload="bup"
+              :on-change="onchange"
+            >
+              <el-button size="small" type="primary">选择excel文件</el-button>
+              <div slot="tip" class="el-upload__tip"></div>
+            </el-upload>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple-light">
+            <span style="font-size:12px"
+              ><u
+                ><i>{{ filepath }}</i></u
+              ></span
+            >
+          </div>
+        </el-col>
+      </el-row>
+      <el-row class="mb">
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
+            <label
+              for="outPath"
+              class="el-button el-button--primary el-button--small"
+            >
+              选择输出文件夹
+              <input
+                id="outPath"
+                type="file"
+                style="display:none"
+                @change="changepath"
+                webkitdirectory
+              />
+            </label>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple-light">
+            <span style="font-size:12px"
+              ><u
+                ><i>{{ filepathout }}</i></u
+              ></span
+            >
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" class="mb" v-if="sheets.length !== 0">
+        <el-col :span="5">
+          <div class="grid-content bg-purple">选择sheet：</div>
+        </el-col>
+        <el-col :span="15">
+          <div class="grid-content bg-purple">
+            <el-checkbox
+              :indeterminate="isIndeterminate"
+              v-model="checkAll"
+              @change="handleCheckAllChange"
+              >全选</el-checkbox
+            >
+            <el-checkbox-group
+              v-model="checkedsheet"
+              @change="handleCheckedSheetChange"
+            >
+              <el-checkbox
+                class="fonts"
+                v-for="item in sheets"
+                :label="item"
+                :key="item"
+                >{{ item }}</el-checkbox
+              >
+            </el-checkbox-group>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row class="mb" v-if="sheets.length !== 0">
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
+            <el-button
+              @click="outMe"
+              type="primary"
+              :disabled="this.checkedsheet.length == 0 || !this.filepathout"
+              :loading="isoutloading"
+              >导出</el-button
+            >
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+    </div>
+    <div>
+      <ul class="infinite-list" style="overflow:auto">
+        <li v-for="i in message" class="infinite-list-item tal" :key="i">
+          {{ i }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import { FileClassJson } from "@/util/FileClass";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      isusereg: true,
+      message: [],
+      filepath: "",
+      filepathout: "",
+      loading: false,
+      keyv: "",
+      keyreg: "",
+      checkedsheet: [],
+      sheets: [],
+      isIndeterminate: true,
+      checkAll: true,
+      isoutloading: false,
+      fobj: ""
+    };
+  },
+  methods: {
+    onchange(e) {
+      this.loading = true;
+      setTimeout(() => {
+        this.filepath = e.raw.path;
+        this.fobj = new FileClassJson(
+          e.raw.path,
+          "/Users/chenlei/Desktop/tt",
+          this.keyv ? this.keyv.toUpperCase() : "A"
+        );
+        this.sheets = this.fobj.workbook.SheetNames;
+        this.checkedsheet = this.fobj.workbook.SheetNames;
+        this.loading = false;
+      }, 500);
+    },
+    handleCheckedSheetChange() {},
+    handleCheckAllChange(val) {
+      this.checkedsheet = val ? this.sheets : [];
+      this.isIndeterminate = false;
+    },
+    changepath(e) {
+      this.filepathout = e.target.files[0].path;
+      this.fobj.setoutPath = this.filepathout;
+    },
+    bup() {
+      return false;
+    },
+    outMe() {
+      this.isoutloading = true;
+      this.fobj.checkSheet(
+        this.checkedsheet,
+        this.isusereg,
+        this.keyv,
+        this.isusereg ? (!this.keyreg ? "&" : this.keyreg) : ""
+      );
+      this.isoutloading = false;
+      this.message = this.fobj.repeatkey;
+    }
+  },
+  mounted() {
+    // message.sub('getRepeat',{})
+  },
+  destroyed() {
+    // message.removeSub()
   }
 };
 </script>
+<style lang="scss" scoped>
+.mb {
+  margin-bottom: 18px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+}
+.bg-purple {
+  text-align: left;
+}
+.fonts {
+  font-size: 12px !important;
+}
+.home {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  // overflow: hidden;
+  & > div {
+    flex: 1;
+  }
+}
+.infinite-list {
+  background-color: #f9f9f9;
+}
+</style>
